b36d9c545e6b2d1def1c10eabfce4f3cf8c37061
TechhTani <natan_ola@hotmail.com>
2013-11-17 14:30:35 +0000

    Byrjadur a TicTacToe og kominn med main klasa og klasa sem er fyrir leikinn og reglurnar.

--- src/main/java/is/ru/SidannaVerkefni/TicTacToeGame.java
+++ src/main/java/is/ru/SidannaVerkefni/TicTacToeGame.java
@@ -5,5 +5,60 @@ public class TicTacToeGame {
 	
 	public TicTacToeGame(){
 	}
+
+	public TicTacToeGame(char[][] t){
+		table = t;
+	}
+	
+	public char[][] getTable(){
+		return table;
+	}
+	
+	public void setTable(char[][] t){
+		table = t;
+	}
+	
+	public boolean CheckWin(char playerSymbol) //PlayerSymbol = X or O
+	{
+		if(table[0][0] == playerSymbol && table[0][1] == playerSymbol && table[0][2] == playerSymbol) //Top Row
+			return true;
+		else if(table[1][0] == playerSymbol && table[1][1] == playerSymbol && table[1][2] == playerSymbol) //Middle Row
+			return true;
+		else if(table[2][0] == playerSymbol && table[2][1] == playerSymbol && table[2][2] == playerSymbol) //Bottom Row
+			return true;
+		else if(table[0][0] == playerSymbol && table[0][1] == playerSymbol && table[0][2] == playerSymbol) //Left Column
+			return true;
+		else if(table[1][0] == playerSymbol && table[1][1] == playerSymbol && table[1][2] == playerSymbol) //middle Column
+			return true;
+		else if(table[2][0] == playerSymbol && table[2][1] == playerSymbol && table[2][2] == playerSymbol) //right Column
+			return true;
+		else if(table[0][0] == playerSymbol && table[1][1] == playerSymbol && table[2][2] == playerSymbol) //Left-To-Right diagonal line
+			return true;
+		else if(table[0][2] == playerSymbol && table[1][1] == playerSymbol && table[2][0] == playerSymbol) //Right-To-Left diagonal line
+			return true;
+			
+		return false; //If no row or column has same character. then the game is no over.
+	}
+	
+	public void addToGrid(char id, char mark) {
+        if (id == '1')
+            table[0][0] = mark;
+        else if (id == '2')
+        	table[0][1] = mark;
+        else if (id == '3')
+        	table[0][2] = mark;
+        else if (id == '4')
+        	table[1][0] = mark;
+        else if (id == '5')
+        	table[1][1] = mark;
+        else if (id == '6')
+        	table[1][2] = mark;
+        else if (id == '7')
+        	table[2][0] = mark;
+        else if (id == '8')
+        	table[2][1] = mark;
+        else if (id == '9')
+        	table[2][2] = mark;
+    }
 	
 }
---
+++ src/main/java/is/ru/SidannaVerkefni/main.java
@@ -0,0 +1,10 @@
+package TicTacToe;
+import TicTacToeGame;
+
+public class main {
+	public static void main(String[] args){
+		
+		TicTacToeGame game = new TicTacToeGame();
+		System.out.print("hello");	
+	}
+}
